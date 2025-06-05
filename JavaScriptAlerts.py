759299e9fd8760ca5d28e9b6a27df092fe21dd7d
Vigneswaran S <126574086+VigneswaranVicky-S@users.noreply.github.com>
2025-06-05 12:33:08 +0530

    initial commit

--- JavaScriptAlerts.py
+++ JavaScriptAlerts.py
@@ -5,14 +5,14 @@ from selenium.webdriver.common.by import By
 
 browser = webdriver.Firefox()
 browser.maximize_window()
-url = ""
+url = "https://the-internet.herokuapp.com/javascript_alerts"
 browser.get(url)
-alert_button = browser.find_element(By.XPATH,"")
+alert_button = browser.find_element(By.XPATH,"//button[normalize-space()='Click for JS Prompt']")
 alert_button.click()
 alert = browser.switch_to.alert
 alert_text = alert.text
 print(alert_text)
 time.sleep(5)
-alert.send_keys("I Love Samuthra")
+alert.send_keys("Selenium with python")
 time.sleep(5)
 alert.dismiss()
